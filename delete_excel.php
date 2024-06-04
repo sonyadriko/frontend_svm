240b3cd8d4b4263de10a12c01932e1e1b13837f5
renataalexandria <xnightsky2008@gmail.com>
2024-06-04 16:39:19 +0700

    push

---
+++ delete_excel.php
@@ -0,0 +1,20 @@
+<?php
+// File path to the Excel file to be deleted
+$file_path = '../backend/data_tweet.csv';
+
+// Check if the file exists before attempting to delete it
+if (file_exists($file_path)) {
+    // Attempt to delete the file
+    if (unlink($file_path)) {
+        $response = array('success' => true);
+    } else {
+        $response = array('success' => false);
+    }
+} else {
+    $response = array('success' => false);
+}
+
+// Return the response as JSON
+header('Content-Type: application/json');
+echo json_encode($response);
+?>
